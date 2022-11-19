76da5b02be17988c27f67ce827d076d8cc50a056
Petr Lorenc <petr.lorenc.ml@gmail.com>
2022-11-19 19:11:23 +0100

    FEAT: better visualisation, preparation for backward

--- minitorch/tensors.py
+++ minitorch/tensors.py
@@ -1,7 +1,18 @@
 from __future__ import annotations # allow add typing hints for classes inside each others
 from typing import List
+from enum import Enum
+import random
 
 from abc import ABC, abstractmethod
+
+
+class OperationTypes(Enum):
+    NO_OPERATION = "INPUT"
+    ADDITION = " + "
+    MULTIPLICATION = " * "
+
+    def __str__(self):
+        return self.value
 
 
 class MyObject(ABC):
@@ -23,34 +34,56 @@ class MyObject(ABC):
 
 class Node(MyObject):
 
-    def __init__(self, value: float, references: List[Node] = None):
+    def __init__(self, value: float, references: List[Node] = None, operation_type: OperationTypes = OperationTypes.NO_OPERATION):
         self.value = value
         self.references = references if references is not None else []
+        self.operation_type = operation_type
+        self.gradient = 1. # gradient for final node, others will be updated after calling backward()
 
-    def __call__(self, *args, **kwargs):
+        self._id = str(round(random.random(), 5)).split(".")[1] # just for visualisation to have better idea about REF
+
+    # overloading () operation
+    def __call__(self, *args, **kwargs) -> float:
         return self.value
 
-    def __add__(self, other: Node):
-        return Node(value=self() + other(), references=[self, other])
+    # overloading + operation
+    def __add__(self, other: Node) -> Node:
+        return Node(value=self() + other(), references=[self, other], operation_type=OperationTypes.ADDITION)
 
-    def __mul__(self, other: Node):
-        return Node(value=self()*other(), references=[self, other])
+    # overloading * operation
+    def __mul__(self, other: Node) -> Node:
+        return Node(value=self() * other(), references=[self, other], operation_type=OperationTypes.MULTIPLICATION)
 
-    def __repr__(self):
-        return f"Node({self.value}) with references to {[str(x) for x in self.references]}"
+    # for showing purposes
+    def __repr__(self, prefix=""):
+        output_repr = f"{prefix} Node({self.value}, id={self._id}) was created by {self.operation_type}"
+        output_repr += "\n"
+        for ref in self.references:
+            output_repr += prefix + ref.__repr__(prefix=prefix + " # ")
+        output_repr += ""
+        return output_repr
 
     def backward(self):
-        return 0
+        pass
+
+    def derivative(self, previous_gradient) -> float:
+        """
+        Calculate gradient of current Node - influence of current Node on final output
+        We will use chain rule and work backward
+        :return:
+        """
+        return
 
 
 if __name__ == '__main__':
-    x = Node(1)
-    y = Node(4)
-    z = Node(5)
+    i = Node(2)
+
+    x = Node(3)
+    y = Node(4) + i
+    z = Node(5) + Node(3) + i
 
     xy = x + y
     xyz = xy * z
 
-    print(type(xyz))
     print(xyz)
 
