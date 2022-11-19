bad337e70de0d713c6a1c01e208e4d37f2fdad65
Petr Lorenc <petr.lorenc.ml@gmail.com>
2022-11-19 14:37:39 +0100

    INIT: basic structure and README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# MiniTorch
+
+The idea for this repository came after seeing [Andrej Karpathy's explanation](https://www.youtube.com/watch?v=VMj-3S1tku0) of his project [micrograd](https://github.com/karpathy/micrograd). I was trying to absorb as many ideas from his tutorial as possible and then make myself passionate about replicating similar ideas. I admit that I may have been influenced by watching his tutorial. Still, I haven't browsed the repository too much. I am also planning to avoid a sneak peek into his code to make my variation of the whole idea. My motto is: Let's surprise myself :)
+
+### Plans
+
+Classes to be implemented:
+
+```python
+Scalar(float)
+Vector(List[Scalar])
+NeuralNet(List[Vector])
+```
+
+Method/principles need to be taken into account:
+
+```python
+forward_pass -> __call__()
+backward_pass -> backward()
+visualize
+train
+```
+
+### How to run the code
+```
+TODO
+```
+
+### Running tests
+```
+TODO
+```
---
+++ minitorch/tensors.py
@@ -0,0 +1,54 @@
+from __future__ import annotations # allow add typing hints for classes inside each others
+from typing import List
+
+from abc import ABC, abstractmethod
+
+
+class MyObject(ABC):
+    """
+    Object which should be parent to all used objects
+    """
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def backward(self):
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+
+class Node(MyObject):
+
+    def __init__(self, value: float, references: List[Node] = None):
+        self.value = value
+        self.references = references
+        self.weight = 1
+
+    def __call__(self, *args, **kwargs):
+        return self.value
+
+    def __add__(self, other: Node):
+        return Node(value=self() + other(), references=[self, other])
+
+    def __repr__(self):
+        return str(self.value)
+
+    def backward(self):
+        return 0
+
+
+if __name__ == '__main__':
+    x = Node(1)
+    y = Node(4)
+    z = Node(5)
+
+    xy = x + y
+    xyz = xy + z
+
+    print(type(xyz))
+    print(xyz)
+
