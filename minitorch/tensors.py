a6aa255ee262cd0b3876b2d557f84d400ec3af46
Petr Lorenc <petr.lorenc.ml@gmail.com>
2022-11-26 21:35:55 +0100

    FEAT: supporting tanh and sigmoid

--- minitorch/tensors.py
+++ minitorch/tensors.py
@@ -1,34 +1,27 @@
-from __future__ import annotations # allow add typing hints for classes inside each others
-from typing import List
+from __future__ import annotations  # allow add typing hints for classes inside each others
+from typing import List, Union, Tuple
 from enum import Enum
 import random
 
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractstaticmethod
 
 
-class OperationTypes(ABC):
-
+class Operation(ABC):
     def __init__(self, value: float):
         self.value = value
 
     def __str__(self):
         return self.__class__.__name__
 
+
+class BinaryOperation(Operation):
+
     @abstractmethod
-    def derivative(self, left_node: Node, right_node: Node):
+    def derivative(self, left_node: Node, right_node: Node) -> Tuple[float, float]:
         pass
 
 
-class InputNode(OperationTypes):
-
-    def derivative(self, left_node: Node, right_node: Node):
-        return 0., 0.
-
-    def __str__(self):
-        return " << "
-
-
-class AddOperation(OperationTypes):
+class AddOperation(BinaryOperation):
 
     # if f = 2x + 3y then
     # df/dx = 2
@@ -42,21 +35,21 @@ class AddOperation(OperationTypes):
         return " + "
 
 
-class SubtractOperation(OperationTypes):
+class SubtractOperation(BinaryOperation):
 
     # if f = 2x + 3y then
     # df/dx = 2
     # if L = 4f + 5y then
     # dL/df = 4
     # together dL/dx = dL/df * df/dx (chain rule) = 4 * 2 = 8
-    def derivative(self, left_node: Node, right_node: Node):
+    def derivative(self, left_node: Node, right_node: Node) -> Tuple[float, float]:
         return 1., -1.
 
     def __str__(self):
         return " - "
 
 
-class ProductOperation(OperationTypes):
+class ProductOperation(BinaryOperation):
 
     def derivative(self, left_node: Node, right_node: Node):
         return right_node.operation.value, left_node.operation.value
@@ -65,31 +58,70 @@ class ProductOperation(OperationTypes):
         return " * "
 
 
-class MyObject(ABC):
+class UnaryOperation(Operation):
+
+    @abstractmethod
+    def derivative(self, root_node: Node) -> float:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def apply(onto: Node, *args, **kwargs) -> Node:
+        pass
+
+    def __str__(self):
+        return " UnaryOperation "
+
+
+class InputNode(Operation):
+
+    def derivative(self, _node: Node) -> float:
+        return 0.
+
+    def __str__(self):
+        return " << "
+
+
+class SigmoidOperation(UnaryOperation):
+
+    @staticmethod
+    def apply(onto: Node, *args, **kwargs) -> Node:
+        import math
+        return Node(references=[onto], operation=SigmoidOperation(value=(1 / (1 + math.exp(-onto.operation.value)))) )
+
+    def derivative(self, _node: Node) -> float:
+        return self.value * (1 - self.value)
+
+    def __str__(self):
+        return " sigmoid() "
+
+
+class TanhOperation(UnaryOperation):
+
+    @staticmethod
+    def apply(onto: Node, *args, **kwargs) -> Node:
+        import math
+        return Node(references=[onto], operation=TanhOperation(value=math.tanh(onto.operation.value)))
+
+    def derivative(self, _node: Node):
+        return 1 - (self.value * self.value)
+
+    def __str__(self):
+        return " tanh() "
+
+
+class Node:
     """
-    Object which should be parent to all used objects
+    Main object which is using for calculation and also for storing reference to other nodes. It also saves the gradient.
     """
-    @abstractmethod
-    def __call__(self, *args, **kwargs):
-        pass
 
-    @abstractmethod
-    def backward(self):
-        pass
-
-    @abstractmethod
-    def __repr__(self):
-        pass
-
-
-class Node(MyObject):
-
-    def __init__(self, references: List[Node] = None, operation: OperationTypes = None, label: str = None):
+    def __init__(self, references: List[Node] = None, operation: Operation = None, label: str = None):
         self.operation = operation
         self.references = references if references is not None else []
         self.gradient = 0.0
 
-        self._id = str(round(random.random(), 5)).split(".")[1] if not label else label # just for visualisation to have better idea about REF
+        self._id = str(round(random.random(), 5)).split(".")[
+            1] if not label else label  # just for visualisation to have better idea about REF
 
     # overloading () operation
     def __call__(self, *args, **kwargs) -> float:
@@ -122,12 +154,17 @@ class Node(MyObject):
             We will use chain rule and work backward
             :return:
         """
-        self.gradient += current_derivative # need to sum it up - but then need something like zero_grad()
+        self.gradient += current_derivative  # need to sum it up - but then need something like zero_grad()
 
         if len(self.references) == 2:
+            self.operation: BinaryOperation
             partial_derivative_left, partial_derivative_right = self.operation.derivative(self.references[0], self.references[1])
             self.references[0].backward(partial_derivative_left * self.gradient)
             self.references[1].backward(partial_derivative_right * self.gradient)
+        elif len(self.references) == 1:
+            self.operation : UnaryOperation
+            partial_derivative = self.operation.derivative(self.references[0])
+            self.references[0].backward(partial_derivative * self.gradient)
         elif len(self.references) == 0:
             pass
 
@@ -140,7 +177,20 @@ if __name__ == '__main__':
     b = Node(operation=InputNode(4), label="b")
 
     o = (x1 * w1) - (x2 * w2) + b
-    print(o)
-    o.backward()
-    print(o)
+    out = TanhOperation.apply(onto=o)
+    print(out)
+    out.backward()
+    print(out)
+    print("#" * 100)
 
+    x1 = Node(operation=InputNode(2), label="x1")
+    w1 = Node(operation=InputNode(3), label="w1")
+    x2 = Node(operation=InputNode(2), label="x2")
+    w2 = Node(operation=InputNode(3), label="w2")
+    b = Node(operation=InputNode(4), label="b")
+
+    o = (x1 * w1) - (x2 * w2) + b
+    out = SigmoidOperation.apply(onto=o)
+    print(out)
+    out.backward()
+    print(out)
